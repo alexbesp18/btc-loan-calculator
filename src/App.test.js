fc8015fc53b25e98236583ecc1de8fed4d38dbaa
alexbesp18 <ab00477@icloud.com>
2025-06-05 00:07:07 -0500

    Add password protection with PayPal link

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders main heading', () => {
+test('renders main heading when authorized', () => {
+  window.localStorage.setItem('authorized', 'true');
   render(<App />);
   const headingElement = screen.getByText(/Inputs & Controls/i);
   expect(headingElement).toBeInTheDocument();
+  window.localStorage.removeItem('authorized');
 });
